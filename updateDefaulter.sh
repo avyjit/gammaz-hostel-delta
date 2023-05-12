20a2f5600ac87a4a9634844cf49d233178e8c6c0
Avyyukt Ajith <avyyukt.ajith@gmail.com>
2023-05-12 12:44:21 +0545

    updateDefaulter.sh

--- updateDefaulter.sh
+++ updateDefaulter.sh
@@ -3,10 +3,20 @@
 semester=$1
 deadline=$2
 
+touch paid.tmp.txt
+
 for student in $(find . -name userDetails.txt); do
     feesFile=$(echo $student | sed -e 's/userDetails.txt/fees.txt/')
     rollno=$(tail -1 $student | cut -d' ' -f2)
     if [ ! $semester -eq $(tail +6 "$feesFile" | wc -l) ]; then
+        #echo adding $rollno to defaulters due to sem
         echo $rollno >> ./feeDefaulters.txt
-    fi 
+    elif [[ $(tail -1 $feesFile) > $deadline ]]; then
+        #echo adding $rollno to defaulters due to deadline
+        echo $rollno >> ./feeDefaulters.txt
+    else
+        echo $rollno $(tail -1 $feesFile) >> ./paid.tmp.txt
+    fi
 done
+
+sort -k2 paid.tmp.txt | head -n 5 >> announcements.txt
