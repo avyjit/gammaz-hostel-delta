9c0d7133552901a8465515196b12c4bf26c2979c
Avyyukt Ajith <avyyukt.ajith@gmail.com>
2023-05-14 16:16:13 +0530

    fix hideous /dev/stdin bug

--- supermode/approveRequests.sh
+++ supermode/approveRequests.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
+# So we're duplicating the stdin to fd 4
+exec 4<&0
+
 while read -r -a line; do 
     name=${line[0]}
     rollno=${line[1]}
     returnDate=${line[2]}
+    echo Preparing to approve student $name
 
-    read -r -p "Approve $rollno $name to return on $returnDate? (Y/N): " approval
+    echo -n "Approve $rollno $name to return on $returnDate? (Y/N): " 
+    # and reading from the duplicate stdin cuz
+    # the current stdin is reading from the file
+
+    # IT TOOK ME 2 HOURS TO FIGURE THIS OUT
+    # I HATE BASH WITH A PASSION
+    # I WOULD'VE DONE THIS SHIT IN 10 MINS WITH PYTHON
+    # MYYYYY GOOOOOOOOOOOOOD
+    # but hey it works now
+    # oh hi mentor going through my code :)
+    read -r approval <&4
     if [[ $approval == "Y" || $approval == "y" ]]; then
         echo "$name $rollno $returnDate" >> approvedSignOutRequests.txt
     fi
-done <<< approvalRequests.txt
+done < approvalRequests.txt
 
 # Once approvals are done, we clear the current "queue"
 > approvalRequests.txt
