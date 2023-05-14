1cfa33747ade38b13ee7be5bdb0ff76ad3acf946
Avyyukt Ajith <avyyukt.ajith@gmail.com>
2023-05-14 16:34:17 +0530

    basic sign out

---
+++ supermode/checkSignOut.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+checkSignOut() {
+    # Check if there are any sign out requests at all
+    # We do this by checking if signOutRequests.txt is non-empty
+    lineCount=$(wc -l signOutRequests.txt | cut -d' ' -f1)
+
+    # If lineCount is not 0, check whether it is present
+    # in the warden's approved requests
+    if [ "$lineCount" != 0 ]; then
+        lastRequest=$(tail -1 signOutRequests.txt)
+        echo Checking last signout request $lastRequest
+        hostelname=$(tail -1 userDetails.txt | cut -d' ' -f5)
+
+        # Check if this is present in the approved requests
+        if grep -q $lastRequest /home/$hostelname/approveSignOutRequests.txt; then
+            echo "Request was approved!"
+        else
+            # Add to defaulters
+            echo $lastRequest >> /home/$hostelname/signOutDefaulters.txt
+        fi
+    else
+        echo "No signout requests!"
+    fi
+
+}
+
+checkSignOut
